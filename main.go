69198a09eaa48bb97f8ac87afa59a3c78c228868
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2021-04-03 13:07:54 +0900

    refactor: 💡 remove redundant Scan

--- main.go
+++ main.go
@@ -6,23 +6,24 @@ import (
 )
 
 func main() {
+	questions := []string{
+		"[類] %s は何の一種か？\n",
+		"[種差] %s は、同じグループの中で他と何が違うのか？\n",
+		"[部分] %s を構成する部分を列挙すると？\n",
+		"[定義] %s とは何か？\n",
+		"[語源] %s の語源は？\n",
+		"[相反] %s の反対は？\n",
+		"[原因・由来] %s を生じさせる（た）ものは？\n",
+	}
+
 	if len(os.Args) != 2 {
 		return
 	}
+
 	target := os.Args[1]
 	var answer string
-	fmt.Printf("[類] %s は何の一種か？\n", target)
-	fmt.Scan(&answer)
-	fmt.Printf("[種差] %s は、同じグループの中で他と何が違うのか？\n", target)
-	fmt.Scan(&answer)
-	fmt.Printf("[部分] %s を構成する部分を列挙すると？\n", target)
-	fmt.Scan(&answer)
-	fmt.Printf("[定義] %s とは何か？\n", target)
-	fmt.Scan(&answer)
-	fmt.Printf("[語源] %s の語源は？\n", target)
-	fmt.Scan(&answer)
-	fmt.Printf("[相反] %s の反対は？\n", target)
-	fmt.Scan(&answer)
-	fmt.Printf("[原因・由来] %s を生じさせる（た）ものは？\n", target)
-	fmt.Scan(&answer)
+	for _, question := range questions {
+		fmt.Printf(question, target)
+		fmt.Scan(&answer)
+	}
 }
