2ae25cd724b1ebd5e748fc71f2b299119557eb96
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2021-04-04 11:08:59 +0900

    refactor: capitalize label of question

--- main.go
+++ main.go
@@ -27,35 +27,35 @@ func generateQuestions(prompts []Prompt, target string) []*survey.Question {
 func main() {
 	prompts := []Prompt{
 		{
-			"genre",
+			"Genre",
 			"[類] %s は何の一種か？\n",
 		},
 		{
-			"difference",
+			"Difference",
 			"[種差] %s は、同じグループの中で他と何が違うのか？\n",
 		},
 		{
-			"part",
+			"Part",
 			"[部分] %s を構成する部分を列挙すると？\n",
 		},
 		{
-			"definition",
+			"Definition",
 			"[定義] %s とは何か？\n",
 		},
 		{
-			"etymology",
+			"Etymology",
 			"[語源] %s の語源は？\n",
 		},
 		{
-			"opposite",
+			"Opposite",
 			"[相反] %s の反対は？\n",
 		},
 		{
-			"cause",
+			"Cause",
 			"[原因・由来] %s を生じさせる（た）ものは？\n",
 		},
 		{
-			"effect",
+			"Effect",
 			"[結果・派生] %s から生じる（た）ものは？\n",
 		},
 	}
